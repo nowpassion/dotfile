a3f2e8d1e4c50cd1607f441eea9ac54a3b15e3b4
nowpassion <billonbill@naver.com>
2025-12-11 01:25:22 +0900

    github_copilot 활성화 여부 코드 수정

--- nvim/lua/plugin_settings/github_copilot.lua
+++ nvim/lua/plugin_settings/github_copilot.lua
@@ -3,14 +3,13 @@ local github_copilot_authinfo = vim.env.HOME .. "/.config/github-copilot/apps.js
 local file = io.open(github_copilot_authinfo, "r")
 if file then
 	local content = file:read("*a")
-	file:close()
 
-	if content:find('"oauth_token"') then
-		vim.b.copilot_enabled = true
-	else
-		vim.b.copilot_enabled = false
+	if content:find('"oauth_token"') == nil then
+		vim.g.loaded_copilot = 1;
 	end
+
+	file:close()
 else
-	vim.b.copilot_enabled = false
+	vim.g.loaded_copilot = 1;
 end
 
